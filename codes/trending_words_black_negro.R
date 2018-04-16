a59362cba40eb75b72c9cd234879029f1095f5ea
Kushal K Dey <kshldey@gmail.com>
2018-04-16 12:27:58 -0500

    opposite trending analysis

---
+++ codes/trending_words_black_negro.R
@@ -0,0 +1,61 @@
+
+
+df <- get(load(file = "../output/black_words_assoc.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = FALSE)[1:100]]
+z2 <- df[,3]/df[,4]
+rownames(df)[order(z2, decreasing = FALSE)[1:100]]
+
+
+df <- get(load(file = "../output/negro_words_assoc.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = FALSE)[1:100]]
+#z2 <- df[,3]/df[,4]
+#rownames(df)[order(z2, decreasing = FALSE)[1:100]]
+
+df <- get(load(file = "../output/negro_words_assoc_BL_ND.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = FALSE)[1:100]]
+#z2 <- df[,3]/df[,4]
+#rownames(df)[order(z2, decreasing = FALSE)[1:100]]
+
+
+
+df <- get(load(file = "../output/negro_words_assoc.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = TRUE)[1:100]]
+
+df <- get(load(file = "../output/negro_words_assoc_BL_ND.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = TRUE)[1:100]]
+
+
+
+df <- get(load(file = "../output/black_words_assoc.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = FALSE)[1:100]]
+
+df <- get(load(file = "../output/black_words_assoc_BL_ND.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = FALSE)[1:100]]
+
+
+
+
+
+
+df <- get(load(file = "../output/negros_words_assoc.rda"))
+
+z1 <- df[,1]/df[,2]
+rownames(df)[order(z1, decreasing = FALSE)[1:100]]
+z2 <- df[,3]/df[,4]
+rownames(df)[order(z2, decreasing = FALSE)[1:100]]
+
+
